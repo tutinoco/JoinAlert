39034320ac7daeeec9a57815f772f5b236efd52a
tutinoco <m@tutino.co>
2022-12-15 09:35:45 +0900

    JoinAleat 初めてのコミット

---
+++ JoinAlert.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class JoinAlert : UdonSharpBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if ( Networking.LocalPlayer != player ) audioSource.Play();
+    }
+}
